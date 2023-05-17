82278d391399771e8850fcc3e8af599458a97e89
vivi108 <sunmyoung61@gmail.com>
2023-05-17 23:06:47 +0900

    init
    Co-authored-by: chromato99 <chromato99@gmail.com>

---
+++ SeoulAPI.js
@@ -0,0 +1,45 @@
+var request = require('request');
+var mysql = require('mysql2');
+var Iconv = require('iconv').Iconv;
+var iconv = new Iconv('EUC-KR', 'UTF-8');
+
+var connection = mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_ROOT_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    insecureAuth: true
+
+});
+
+connection.connect();
+
+var url = 'http://openapi.seoul.go.kr:8088/65595a50536368723130314658624a4f/json/CardBusTimeNew/1/5/201511';
+
+request({
+    url: url,
+    method: 'GET',
+
+}, function (error, response, body) {
+    //console.log('Status', response.statusCode);
+    //console.log('Headers', JSON.stringify(response.headers));
+    //console.log('Response received', body);
+    body = JSON.parse(body);
+    console.log(body.CardBusTimeNew.row);
+    body.CardBusTimeNew.row.forEach(element => {
+        connection.query('INSERT INTO seoulRide SET ?', element, function (error, results, fields) {
+            if (error) throw error;
+            console.log('The solution is: ', results);
+            connection.query('SELECT * FROM seoulRide', function (error, results, fields) {
+                if (error) throw error;
+                console.log('This is SELECT QUERY!! : ', results);
+            });
+        }
+        );
+    });
+
+
+
+
+});
+
